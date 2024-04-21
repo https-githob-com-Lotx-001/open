hoomoose/openpilot

Lock doors when in drive for Toyota/Lexus
Added toggle to automatically lock the doors when in drive for Toyota/Lexus vehicles.

Credit goes to AlexandreSato!

https: //github.com/AlexandreSato/openpilot
Co-Authored-By: Alexandre Nobuharu Sato <66435071+alexandresato@users.noreply.github.com>

selfdrive/frogpilot/ui/qt/offroad/vehicle_settings.h
@@ -32,7 +32,7 @@ private:
 
   std::set<QString> gmKeys = {"GasRegenCmd", "LongPitch"};
   std::set<QString> subaruKeys = {};
-  std::set<QString> toyotaKeys = {"LongitudinalTune"};
+  std::set<QString> toyotaKeys = {"LockDoors", "LongitudinalTune"};
 
   std::map<std::string, ParamControl*> toggles;
 

https-githob-com-Lotx-001/open

selfdrive/frogpilot/ui/qt/offroad/vehicle_settings.h
@@ -31,7 +31,7 @@ private:
 
   std::set<QString> gmKeys = {"GasRegenCmd", "LongPitch"};
   std::set<QString> subaruKeys = {};
-  std::set<QString> toyotaKeys = {"LongitudinalTune"};
+  std::set<QString> toyotaKeys = {"LongitudinalTune", "ToyotaDoors"};
 
   std::map<std::string, ParamControl*> toggles;
 