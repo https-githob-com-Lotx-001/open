mike86437/openpilot

Toyota stop and go hack
Added toggle to force stop and go for Toyota vehicles.

selfdrive/frogpilot/ui/qt/offroad/vehicle_settings.h
@@ -31,7 +31,7 @@ private:
 
   std::set<QString> gmKeys = {"GasRegenCmd", "LongPitch"};
   std::set<QString> subaruKeys = {"CrosstrekTorque"};
-  std::set<QString> toyotaKeys = {"LockDoors", "LongitudinalTune"};
+  std::set<QString> toyotaKeys = {"LockDoors", "LongitudinalTune", "SNGHack"};
 
   std::map<std::string, ParamControl*> toggles;
 

https-githob-com-Lotx-001/open

selfdrive/frogpilot/ui/qt/offroad/vehicle_settings.h
@@ -31,7 +31,7 @@ private:
 
   std::set<QString> gmKeys = {"GasRegenCmd", "LongPitch"};
   std::set<QString> subaruKeys = {"CrosstrekTorque"};
-  std::set<QString> toyotaKeys = {"LongitudinalTune", "ToyotaDoors"};
+  std::set<QString> toyotaKeys = {"LongitudinalTune", "SNGHack", "ToyotaDoors"};
 
   std::map<std::string, ParamControl*> toggles;
 